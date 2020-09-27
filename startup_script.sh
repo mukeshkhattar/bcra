3e582e07492019aaf968120d3cdee513791b0171
Mukesh Khattar <61493035+mukeshkhattar@users.noreply.github.com>
2020-09-26 21:28:07 -0700

    update

--- startup_script.sh
+++ startup_script.sh
@@ -7,6 +7,6 @@ virtualenv venv -p python3
 source venv/bin/activate
 pip install -r requirements.txt
 cat gce_backend.py |
-  sed -e "s/YOUR_BACKEND_SERVICE_ID/$(gcloud compute backend-services describe my-backend-service --global --format="value(id)")/g" |
+  sed -e "s/YOUR_BACKEND_SERVICE_ID/$(gcloud compute backend-services describe network-a-us-west-1-backend --global --format="value(id)")/g" |
   sed -e "s/YOUR_PROJECT_ID/$(gcloud config get-value account | tr -cd "[0-9]")/g" > real_backend.py
 gunicorn real_backend:app -b 0.0.0.0:80
